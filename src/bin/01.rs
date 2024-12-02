de1cf6648e54bd956a7ffbf3c1e38b73623d0959
Evgenii Grigorenko <eu.grigorenko@gmail.com>
2024-12-02 16:33:15 +0100

    feat: day 1 | part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,25 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut i = -1;
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .flat_map(|line| line.split_whitespace())
+        .map(|n| n.parse::<u32>().unwrap())
+        .partition(|_| {
+            i += 1;
+            i % 2 == 0
+        });
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let result: u32 = left
+        .into_iter()
+        .zip(right)
+        .map(|(l, r)| (r.abs_diff(l)))
+        .sum();
+
+    Some(result)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -21,6 +39,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
