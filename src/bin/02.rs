74be0ed0c228f47209a30515be63e63f26e976bb
Evgenii Grigorenko <eu.grigorenko@gmail.com>
2024-12-05 11:14:09 +0100

    feat: day 2 | part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,17 +1,31 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().map(|line| is_safe(line) as u32).sum())
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let line = parse_report_levels(line);
+                is_safe(&line) as u32
+            })
+            .sum(),
+    )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let line = parse_report_levels(line);
+                is_safe_2(&line) as u32
+            })
+            .sum(),
+    )
 }
 
-fn is_safe(raw_report: &str) -> bool {
-    let mut levels = raw_report
-        .split_whitespace()
-        .filter_map(|n| n.parse::<i32>().ok());
+fn is_safe<'a>(levels: impl IntoIterator<Item = &'a i32>) -> bool {
+    let mut levels = levels.into_iter();
 
     let Some(first) = levels.next() else {
         return false;
@@ -32,6 +46,27 @@ fn is_safe(raw_report: &str) -> bool {
     })
 }
 
+fn is_safe_2(levels: &[i32]) -> bool {
+    if is_safe(levels) {
+        return true;
+    }
+
+    for n in 0..levels.len() {
+        let skipped = levels.iter().take(n).chain(levels.iter().skip(n + 1));
+        if is_safe(skipped) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn parse_report_levels(line: &str) -> Vec<i32> {
+    line.split_whitespace()
+        .filter_map(|n| n.parse::<i32>().ok())
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,6 +80,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
